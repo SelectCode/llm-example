64892c486afd9793dc517b43173c93666e042a91
Gereon Elvers <gereonelvers99@gmail.com>
2023-07-03 03:02:44 +0200

    Initial commit

--- app.py
+++ app.py
@@ -1,33 +1,17 @@
-from langchain import PromptTemplate, LLMChain
-from langchain.chains import RetrievalQA
+from langchain import PromptTemplate
+from langchain.chains import APIChain
+from langchain.chains.api import open_meteo_docs
 from langchain.chat_models import ChatOpenAI
 
 import chainlit as cl
-
-from utils import get_docsearch
 
 template = """Input: {question}
 Output: Let's think step by step."""
 
 
 @cl.langchain_factory(use_async=True)
-async def factory():
-    files = None
-    while files is None:
-        files = await cl.AskFileMessage(
-            content="Select File",
-            accept=["text/plain", "application/pdf"],
-            max_size_mb=20,
-            timeout=180,
-        ).send()
-    file = files[0]
-    msg = cl.Message(content=f"Processing `{file.name}`...")
-    await msg.send()
-    docsearch = await cl.make_async(get_docsearch)(file)
-    llm_chain = RetrievalQA.from_chain_type(
-        ChatOpenAI(temperature=0, streaming=True),
-        chain_type="stuff",
-        retriever=docsearch.as_retriever(max_tokens_limit=4097),
-    )
-    await msg.update(content=f"`{file.name}` processed. You can now ask questions!")
+def factory():
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm = ChatOpenAI()
+    llm_chain = APIChain.from_llm_and_api_docs(llm, open_meteo_docs.OPEN_METEO_DOCS, verbose=True)
     return llm_chain
