cafc06e173eed426594ae4d7616e9c3923283f19
Gereon Elvers <gereon.elvers@tum.de>
2023-08-10 14:27:39 +0200

    Update app.py and README

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from langchain import PromptTemplate
+from langchain import PromptTemplate, OpenAI, LLMChain
+import chainlit as cl
 from langchain.chains import APIChain
 from langchain.chains.api import open_meteo_docs
 from langchain.chat_models import ChatOpenAI
 
-import chainlit as cl
+template = """Question: {question}
 
-template = """Input: {question}
-Output: Let's think step by step."""
+Answer: Let's think step by step."""
 
 
-@cl.langchain_factory(use_async=True)
-def factory():
-    prompt = PromptTemplate(template=template, input_variables=["question"])
+@cl.on_chat_start
+def main():
     llm = ChatOpenAI()
-    llm_chain = APIChain.from_llm_and_api_docs(llm, open_meteo_docs.OPEN_METEO_DOCS, verbose=True)
-    return llm_chain
+    api_chain = APIChain.from_llm_and_api_docs(llm, open_meteo_docs.OPEN_METEO_DOCS, verbose=True)
+
+    # Store the chain in the user session
+    cl.user_session.set("api_chain", api_chain)
+
+
+@cl.on_message
+async def main(question: str):
+    prompt = PromptTemplate(template=template, input_variables=["question"])
+    llm_chain = cl.user_session.get("llm_chain")  # type: APIChain
+    res = llm_chain.run(prompt.format_prompt(question=question), callbacks=[cl.AsyncLangchainCallbackHandler()])
+    await cl.Message(content=res).send()
